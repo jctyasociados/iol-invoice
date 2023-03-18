961220ee0fef7e4437490202dfd121567b0b3237
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-03-18 16:33:14 +0100

    add last line gunicorn changed port to 5000

--- app.py
+++ app.py
@@ -4448,7 +4448,7 @@ def password_check(password):
     
         
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=5000)
 
                
         
