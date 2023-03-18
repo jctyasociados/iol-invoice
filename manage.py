6c3ab2fd4fab7fd4b88acfcbd800c3808fb68e44
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-03-18 14:09:11 +0100

    app.py host 0.0.0.0 port 3000

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from flask_app import app, db
+from app import app, db
 
 #app.config.from_object(os.environ['APP_SETTINGS'])
 
@@ -13,4 +13,4 @@ manager.add_command('db', MigrateCommand)
 
 
 if __name__ == '__main__':
-    manager.run()
+    manager.run(host='0.0.0.0', port=3000)
