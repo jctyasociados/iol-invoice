977ae8eb301f13235bcef1ea3e8e91a71bfdbd3d
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-04-16 04:21:29 +0200

    upload folder

--- flask_app.py
+++ flask_app.py
@@ -272,7 +272,7 @@ def upload():
                 image.save(os.path.join(app.config['UPLOAD_FOLDER'], finalimagename))
                 width, height = image.size
                    
-            upload_path = "uploads"
+            upload_path = app.config['UPLOAD_FOLDER']
             os.chdir(upload_path)
             os.remove(destination)
             
