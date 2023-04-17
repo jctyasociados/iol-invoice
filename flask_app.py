ef770d847505bbfcc784c45dcf16a9567c008b9c
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-04-17 23:39:38 +0200

    path

--- flask_app.py
+++ flask_app.py
@@ -53,8 +53,8 @@ path = os.getcwd()
 # file Upload
 UPLOAD_FOLDER = os.path.join(path, 'static/uploads')
 
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
+"""if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)"""
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
