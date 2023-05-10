84a511cc92a751b4f89b64893f6e70bffdb63bf7
Javier Cristobal <javier.cristobal.toscano@gmail.com>
2023-05-10 10:17:20 +0200

    lost connection fix

--- flask_app.py
+++ flask_app.py
@@ -33,6 +33,8 @@ app = Flask(__name__,
 
 app.config.from_object(os.getenv('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+app.config['SQLALCHEMY_POOL_TIMEOUT'] = 20
 modus = Modus(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
